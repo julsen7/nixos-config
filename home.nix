5809b4b245ee26b7c17d0f8ff3313ab87f4ccec0
Julsen <263753131+julsen7@users.noreply.github.com>
2026-07-05 23:41:09 +0200

    Fix typo in zsh autosuggestions configuration

--- home.nix
+++ home.nix
@@ -78,7 +78,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
     shellAliases = {
